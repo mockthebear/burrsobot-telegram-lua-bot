eece7170c4cbfd8a5ba86e8295bfcf6c23351145
Matheus Braga <matheus@gocache.com.br>
2021-06-22 12:53:24 -0300

    updating core module

---
+++ modules/core/chat/inspectadm.lua
@@ -0,0 +1,23 @@
+function OnCommand(msg, text, args)
+	if not msg.reply_to_message then 
+		reply("Send this command replying to a message!")
+		return
+	end
+	local m = msg.reply_to_message
+
+	local str = "Elements:"
+	for i,b in pairs(m) do 
+		str = str .. "\n<b>"..i..""
+		if type(b) == "table" then
+			str = str .. "</b><code>"
+			for a,c in pairs(b) do 
+				str = str .. "\n		"..a.." = "..cjson.encode(c):htmlFix()..""
+			end
+			str = str .. "</code>"
+		else 
+			str = str .. " = "..tostring(b):htmlFix().."</b>"
+		end
+		str = str .. "\n----------------------------"
+	end
+	bot.sendMessage(g_chatid,str, "HTML" ,true,false,g_msg.message_id)
+end
